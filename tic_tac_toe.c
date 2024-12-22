26418983cb86b3ed4861c98fc5fe8f14bb9bb91c
Jack <jacktotten@hotmail.co.uk>
2024-12-22 09:47:31 +0000

    creating c file and importing the standard libraries

---
+++ tic_tac_toe.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <time.h>
+
+
+// This is the main loop that is executed when the c file is made.
+int main()
+{
+
+    return 0;
+}
