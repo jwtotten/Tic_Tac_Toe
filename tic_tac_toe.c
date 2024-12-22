1755b080ae5ce7489edc0e58aa22b4330efa98fb
Jack <jacktotten@hotmail.co.uk>
2024-12-22 10:58:31 +0000

    Adding global variables.

--- tic_tac_toe.c
+++ tic_tac_toe.c
@@ -3,10 +3,52 @@
 #include <ctype.h>
 #include <time.h>
 
+char board[3][3];
+const char PLAYER = 'X';
+const char COMPUTER = '0';
+
+void resetBoard();
+void printBoard();
+int checkFreeSpaces();
+void playerMove();
+void computerMove();
+char checkWinner();
+void printWinner(char);
+
 
 // This is the main loop that is executed when the c file is made.
 int main()
 {
 
     return 0;
+}
+
+
+void resetBoard()
+{
+
+}
+void printBoard()
+{
+
+}
+int checkFreeSpaces()
+{
+
+}
+void playerMove()
+{
+
+}
+void computerMove()
+{
+
+}
+char checkWinner()
+{
+
+}
+void printWinner(char winner)
+{
+
 }
