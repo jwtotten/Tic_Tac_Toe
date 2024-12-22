cab69ce5190ffe02809548aeb5d599839d0cc526
Jack <jacktotten@hotmail.co.uk>
2024-12-22 11:17:58 +0000

    Updating .gitignore and adding printing of board function.

--- tic_tac_toe.c
+++ tic_tac_toe.c
@@ -19,6 +19,13 @@ void printWinner(char);
 // This is the main loop that is executed when the c file is made.
 int main()
 {
+    char winner = ' ';
+
+    while(winner  == ' ' && checkFreeSpaces())
+    {
+        resetBoard();
+        printBoard();
+    }
 
     return 0;
 }
@@ -26,11 +33,24 @@ int main()
 
 void resetBoard()
 {
+    for(int i = 0; i < 3; i++)
+    {
+        for(int j = 0; j < 3; j++)
+        {
+            board[i][j] = ' ';
+        }
+    }
 
 }
 void printBoard()
 {
-
+    for(int k = 0; k < 3; k++)
+    {
+        printf("   |   |   ");
+        printf(" %c | %c | %c ", board[k][0], board[k][1], board[k][2]);
+        printf("   |   |   ");
+        printf("\n---|---|---\n");
+    }
 }
 int checkFreeSpaces()
 {
