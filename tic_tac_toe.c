0287c2e04e72421df224b7e1b729d8aa9b2bd9f1
Jack <jacktotten@hotmail.co.uk>
2024-12-23 20:48:21 +0000

    adding second diagonal to check.

--- tic_tac_toe.c
+++ tic_tac_toe.c
@@ -131,6 +131,10 @@ char checkWinner()
         return board[0][0];
     }
     
+    if(board[0][2] == board[1][1] && board[0][2] == board[2][0])
+    {
+        return board[0][2];
+    }
 }
 void printWinner(char winner)
 {
